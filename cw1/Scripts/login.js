75d340142c3db5bdc6977843987baa42c79f787b
celestepru <celesteprussiani@outlook.it>
2022-03-16 12:35:22 +0000

    my json does not work :(

---
+++ cw1/Scripts/login.js
@@ -0,0 +1,21 @@
+﻿function checkJson() {
+    var username = $("#loginUsername").val();
+    var password = $("#loginPassword").val();
+    $.getJSON("/Content/yo.json", function (data) {
+        alert("ok")
+        $.each(data, function (key, val) {
+            alert(val)
+            //if (username == val[0]) {
+            //    if (password == val[1]) {
+            //        alert("Logged!");
+            //    } else {
+            //        alert("Wrong password");
+            //    }
+            //} else {
+            //    alert("User not found");
+            //}
+        });
+    }).fail(function (data) {
+        alert("ERROR getting data")
+    });
+}
