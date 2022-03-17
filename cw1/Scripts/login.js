8c5de11bc3ebd6a72f73edb7e7da92022c229ca9
celestepru <celesteprussiani@outlook.it>
2022-03-17 12:23:00 +0000

    login  with sessionstorage

--- cw1/Scripts/login.js
+++ cw1/Scripts/login.js
@@ -6,33 +6,9 @@
         $("#loginForm").fadeOut(function () {
             $("#loggedForm").fadeIn();
         });
-        setCookie("username", username, 1);
-        localStorage.setItem("user", username);
+        sessionStorage.setItem("user", username);
         $("#welcome").text("Welcome " + username + "!");
     } else if (buffer[0] == "signup") {
         $("#control").text("Nice, you're all signed up! Login below to start.");
     }
 });
-
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
